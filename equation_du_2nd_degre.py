04603b5722beb25bdfacd3e17f7d9a4f77d1070c
Zimouben <149658937+Zimouben@users.noreply.github.com>
2023-11-05 20:19:35 +0100

    Update equation_du_2nd_degre.py

--- equation_du_2nd_degre.py
+++ equation_du_2nd_degre.py
@@ -34,11 +34,11 @@ with st.expander("3 - Instruction 'if', 'else' et 'elif'"):
     st.subheader("Exemple : On cherche à savoir quels sont les nombres premiers compris entre 1 et 10")
     code = '''a=int(input("Valeur de a"))
         if a==5 :
-            print("a est un nombre premier nombres entre 1 et 10")
+            print("a est un nombre premier compris entre 1 et 10")
         elif a==1 :
-            print("a est un nombre premier nombres entre 1 et 10")
+            print("a est un nombre premier compris entre 1 et 10")
         elif a==2 or 3 or 7 :
-            print("a est un nombre premier nombres entre 1 et 10")
+            print("a est un nombre premier compris entre 1 et 10")
         else :
             print("a n'est pas un nombre premier compris entre 1 et 10")'''
     st.code(code, language = 'python')
